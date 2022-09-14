71f1919b9fa762bccfbcedd4a717b076b82a0540
aditya <adityakumar222333@gmail.com>
2022-09-14 22:06:50 +0530

    🚧 Work in Progress:  implemented bare socket.io

--- contexts/messageContext/MessageState.jsx
+++ contexts/messageContext/MessageState.jsx
@@ -1,9 +1,15 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import ACTIONS from "./MessageActions";
 import MessageContext from "./MessageContext";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 const reducer = (state = [], action) => {
   if (action.type === ACTIONS.ADD_MESSAGE) {
+    socket.emit("send-message", action.payload);
+    return [...state, action.payload];
+  }
+  if (action.type === ACTIONS.RECEIVE_MESSAGE) {
     return [...state, action.payload];
   }
   return state;
@@ -11,6 +17,16 @@ const reducer = (state = [], action) => {
 
 const MessageState = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, []);
+  useEffect(() => {
+    // first
+    socket.on("connection", (message) => console.log(message));
+    socket.on("receive-message", (message) =>
+      dispatch({
+        type: ACTIONS.RECEIVE_MESSAGE,
+        payload: { text: message.text, author: "other" },
+      })
+    );
+  }, []);
 
   return (
     <MessageContext.Provider
