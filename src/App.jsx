f06459d8c0ad99209364c3cc0fea7ab87131e0eb
aditya <adityakumar222333@gmail.com>
2022-09-14 20:48:38 +0530

    🚧 Work in Progress:  dispatching message onsubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import ACTIONS from "../contexts/messageContext/MessageActions";
+import messageContext from "../contexts/messageContext/MessageContext";
 import "./App.css";
 import { MessageContainer } from "./components/MessageContainer";
 
 function App() {
   const [message, setMessage] = useState("");
+  const { dispatch } = useContext(messageContext);
 
   const messegeSendHandler = (e) => {
     e.preventDefault();
-
+    dispatch({
+      type: ACTIONS.ADD_MESSAGE,
+      payload: { text: message, author: "me" },
+    });
     // make a new text buble component and append it to the chilren array of thr message container
   };
 
   return (
     <div className="App">
       <button id="start-btn">Generate Room</button>
-      <MessageContainer children={message} />
-      <form onClick={messegeSendHandler} id="message-form">
+      <MessageContainer />
+      <form onSubmit={messegeSendHandler} id="message-form">
         <input
           value={message}
           onChange={(e) => setMessage(e.target.value)}
