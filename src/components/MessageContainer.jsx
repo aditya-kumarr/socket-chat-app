f01a57b29060c4d851dbe1993e8d70c179c3482f
aditya <adityakumar222333@gmail.com>
2022-09-14 21:36:44 +0530

    🐛 FIX: fixed the issues with styling

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,15 +1,38 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import messageContext from "../../contexts/messageContext/MessageContext";
 import TextBubble from "./TextBubble.style";
 
-export function MessageContainer({ children = [""] }) {
-  const populateMessageBox = (message = []) => {
-    message.map((message) => (
-      <TextBubble author={message.author}>{message.text}</TextBubble>
-    ));
-  };
+export function MessageContainer() {
+  const { messages } = useContext(messageContext);
+  console.log("message conatainer says:");
+  console.log(messages);
 
   return (
-    <div className="card" id="text-container">
-      {<TextBubble>{populateMessageBox()}</TextBubble>}
-    </div>
+    <Container id="text-container">
+      {messages.map((message, index) => (
+        <TextBubble key={index} author={message.author}>
+          {message.text}
+        </TextBubble>
+      ))}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  gap: 0.5em;
+  flex-direction: column;
+  padding: 0.25em;
+  border: 1px solid grey;
+  border-radius: 1em;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 400px;
+  width: 300px;
+  & > *:first-child {
+    margin-top: auto !important;
+  }
+`;
