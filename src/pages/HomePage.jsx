582d39d56937b7b5d5600aa7f99b727fef8470a1
aditya <adityakumar222333@gmail.com>
2022-09-17 04:48:56 +0530

    💥 NEW: added the modal component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const HomePage = () => {
+  const [room, setRoom] = useState("");
+  const [roomInput, setRoomInput] = useState("");
+  return <div></div>;
+};
+
+const RoomSelection = styled.div`
+    
+`
+
+export default HomePage;
