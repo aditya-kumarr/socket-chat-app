066bde5259c62028e9ce055a182446edb48bd74f
aditya <adityakumar222333@gmail.com>
2022-09-17 04:07:38 +0530

    💥 NEW: added the global state for webRTC

---
+++ contexts/connectionContext/ConnectionHandler.js
@@ -0,0 +1,70 @@
+export async function setOffer(pc, socket, id) {
+  pc.onicecandidate = (event) => {
+    event.candidate &&
+      socket.emit("setOfferCandidate", event.candidate.toJSON());
+  };
+  pc.dataChannel = pc.createDataChannel("channel");
+  pc.dataChannel.onopen = () => console.log("connected!");
+  pc.dataChannel.onmessage = (message) => console.log(message.data);
+
+  // Create offer
+  const offerDescription = await pc.createOffer();
+  await pc.setLocalDescription(offerDescription);
+
+  const offer = {
+    sdp: offerDescription.sdp,
+    type: offerDescription.type,
+  };
+
+  socket.emit("setOffer", offer, id);
+
+  socket.on("getAnswer", (answer) => {
+    if (!pc.currentRemoteDescription) {
+      const answerDescription = new RTCSessionDescription(answer);
+      pc.setRemoteDescription(answerDescription);
+    }
+    console.log(answer);
+  });
+  socket.on("getAnswerCandidate", (candidateData) => {
+    candidateData.forEach((cand) => {
+      const candidate = new RTCIceCandidate(cand);
+      pc.addIceCandidate(candidate);
+    });
+    console.log("got answer candidates");
+  });
+}
+
+export async function getOffer(pc, socket, id) {
+  console.log(pc.localDescription);
+  socket.emit("getOffer", id);
+  socket.on("offer", async (offer) => {
+    console.log("offer event called");
+    pc.onicecandidate = (event) => {
+      event.candidate &&
+        socket.emit("setAnswerCandidate", event.candidate.toJSON());
+    };
+    const offerDescription = offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    const answerDescription = await pc.createAnswer();
+    console.log("setting local description");
+    await pc.setLocalDescription(answerDescription);
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+
+    // await callDoc.update({ answer });
+    socket.emit("setAnswer", answer);
+    socket.on("getOfferCandidate", (offerCandidates) => {
+      offerCandidates.forEach((offerCandidate) => {
+        pc.addIceCandidate(new RTCIceCandidate(offerCandidate));
+      });
+    });
+    pc.ondatachannel = (e) => {
+      pc.dataChannel = e.channel;
+      pc.dataChannel.onopen = (e) => console.log("connectinon OPENED!");
+      pc.dataChannel.onmessage = (e) => console.log(e.data);
+    };
+  });
+}
---
+++ contexts/connectionContext/ConnectionState.jsx
@@ -0,0 +1,50 @@
+import React, { useReducer, useEffect } from "react";
+import ConnectionContext from "./ConnectionContext";
+import { getOffer, setOffer } from "./ConnectionHandler";
+import { io } from "socket.io-client";
+import ACTIONS from "../Actions";
+const socket = io("http://localhost:3000");
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+const pc = new RTCPeerConnection(servers);
+// let localStream = null;
+// let remoteStream = null;
+
+const reducer = (state = [], action) => {
+  if (action.type === ACTIONS.SEND_OFFER) {
+    // socket.emit("send-message", action.payload);
+    setOffer(pc, socket, action.roomID);
+    return { ...state, pc: pc };
+  }
+  if (action.type === ACTIONS.RECEIVE_OFFER) {
+    getOffer(pc, socket, action.roomID);
+    return { ...state, pc: pc };
+  }
+  return state;
+};
+
+const ConnectionState = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, { pc: pc, socket: socket });
+
+  return (
+    <ConnectionContext.Provider
+      value={{
+        pc: state.pc,
+        socket: state.socket,
+        connectionDispatch: dispatch,
+      }}
+    >
+      {children}
+    </ConnectionContext.Provider>
+  );
+};
+
+export default ConnectionState;
