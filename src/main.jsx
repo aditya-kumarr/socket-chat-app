405606518ee0edf79e3c9723f7c528face6dfc2a
aditya <adityakumar222333@gmail.com>
2022-09-17 07:09:19 +0530

    ✏️ Fix Typo: changed import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GlobalStateContext from "../contexts/GlobalStateContext";
+import GlobalStateContext from "./contexts/GlobalStateContext";
 import App from "./App";
 import "./index.css";
 
