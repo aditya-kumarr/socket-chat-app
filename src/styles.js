6779c68de7a190cac92b408a94937c724b375503
aditya <adityakumar222333@gmail.com>
2022-09-18 04:42:52 +0530

    🔨 Refactor Code:  refactored the styles

--- src/styles.js
+++ src/styles.js
@@ -102,6 +102,27 @@ export const SButton = styled.button`
     max-width: 40vw;
   }
 `;
+
+export const SButton2 = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+
+  &:hover {
+    border-color: #646cff;
+  }
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+`;
+
 export const ButtonGroup = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -125,5 +146,6 @@ export const ButtonGroup = styled.div`
     }
   }
 `;
+
 
 export const STime = styled.time``;
