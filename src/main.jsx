19e0d3ff99760193ffde3a6aee6725b5a6b3264c
aditya <adityakumar222333@gmail.com>
2022-09-18 06:05:09 +0530

    💥 NEW: surrounded the app with browser router

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import GlobalStateContext from "./contexts/GlobalStateContext";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GlobalStateContext>
-    <App />
-  </GlobalStateContext>
+  <BrowserRouter>
+    <GlobalStateContext>
+      <App />
+    </GlobalStateContext>
+  </BrowserRouter>
 );
