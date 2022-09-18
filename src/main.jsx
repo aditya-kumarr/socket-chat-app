ee3ca45faba89396d94ffe5c144038df97d74807
aditya <adityakumar222333@gmail.com>
2022-09-19 05:18:55 +0530

    🐛 FIX: fixed all deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import GlobalStateContext from "./contexts/GlobalStateContext";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <GlobalStateContext>
       <App />
     </GlobalStateContext>
-  </BrowserRouter>
+  </HashRouter>
 );
